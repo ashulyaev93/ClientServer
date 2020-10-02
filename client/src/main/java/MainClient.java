7d6ce9bbfe5cacf9bacd89d66c76a38fb8603615
geekbrains <ashulyaev93@mail.ru>
2020-10-02 23:35:31 +0400

    getFile

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -1,15 +1,29 @@
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.nio.file.Paths;
 
-public class MainClient { //клиентская часть
+public class MainClient {
 
     public static void main(String...str){
         try(Socket socket = new Socket("localhost", 8085)){
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            TempFileMessage tfm = new TempFileMessage(Paths.get("client/exampleTxt.txt"));
+            TempFileMessage tfm = new TempFileMessage(Paths.get("client/client_repository/exampleTxt.txt"));
             out.writeObject(tfm);
+
+            //передача файла;
+            String fileName = "exampleTxt.txt";
+            short fileNameLength = (short) fileName.length();
+            out.writeShort(fileNameLength);
+            out.write(fileName.getBytes());
+            out.writeLong(new File("client/client_repository/"+fileName).length());
+            byte[] buf = new byte[1024];
+            try(InputStream in = new FileInputStream("client/client_repository/"+fileName)){
+                int n;
+                while ((n = in.read(buf)) != -1){
+                    out.write(buf, 0, n);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- server/src/main/java/MainApp.java
+++ server/src/main/java/MainApp.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MainApp {//сервер
+public class MainApp {
     public static void main(String...str){
         try(ServerSocket sc = new ServerSocket(8085)) {
             System.out.println("Server is listening");
@@ -12,6 +11,19 @@ public class MainApp {//сервер
                 byte[] bytes = tfm.getBytes();
                 for(byte b:bytes){
                     System.out.print((char)b);
+                }
+
+                  //передача файла;
+                int x = in.read();
+                short fileNameSize = in.readShort();
+                byte[] fileNameBytes = new byte[fileNameSize];
+                in.read(fileNameBytes);
+                String fileName = new String(fileNameBytes);
+                long fileSize = in.readLong();
+                try(OutputStream out = new BufferedOutputStream(new FileOutputStream("server/server_repository"+fileName))){
+                    for(int i = 0; i < fileSize; i++){
+                        out.write(in.read());
+                    }
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
